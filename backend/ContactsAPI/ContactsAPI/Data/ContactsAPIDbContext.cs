7d1a07b9f603207781cd6fba26a9fa97025ee431
AP <aleksandrpolishchuk@gmail.com>
2023-03-25 20:16:45 +0700

    add ContactsController

--- backend/ContactsAPI/ContactsAPI/Data/ContactsAPIDbContext.cs
+++ backend/ContactsAPI/ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ContactsAPI.Data
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
 {
-    public class ContactsAPIDbContext
+    public class ContactsAPIDbContext : DbContext
     {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
